ac6d87150fa73ba125c49aecc0cfa10132a057e3
AnupaDPerera <anupadilmith@gmail.com>
2026-03-12 13:47:37 +0530

    Updated
    Add some new features.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-function showMessage(){
-alert("Recipe added successfully!");
+document.addEventListener("DOMContentLoaded", function(){
+
+let recipeForm=document.getElementById("recipeForm");
+
+if(recipeForm){
+recipeForm.addEventListener("submit",function(e){
+
+let name=document.getElementById("name").value;
+
+if(name===""){
+alert("Recipe name is required");
+e.preventDefault();
 }
+
+});
+}
+
+let contactForm=document.getElementById("contactForm");
+
+if(contactForm){
+contactForm.addEventListener("submit",function(e){
+
+let email=document.getElementById("email").value;
+
+if(!email.includes("@")){
+alert("Enter valid email");
+e.preventDefault();
+}
+
+});
+}
+
+});
